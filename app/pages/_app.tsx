ee1cdf79642928f7fa67155d9b6ee44ee52760e7
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-01-18 12:43:23 +0530

    config: shadcn

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from "@/components/navbar";
+import { Providers } from "@/components/providers";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Providers>
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <div className="p-6">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </Providers>
+  );
 }
