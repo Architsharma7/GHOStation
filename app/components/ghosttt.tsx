e616d30d6403aa0b82c67eafcb01c3ba23ebbd4c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2024-01-19 19:18:37 +0530

    feat: custom cursor and landing page hero section

---
+++ app/components/ghosttt.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Ghosttt() {
+  return (
+    <div id="ghost">
+      <div className="ghost">
+        <div className="ghost__waves">
+          <div className="ghost__wave"></div>
+          <div className="ghost__wave"></div>
+        </div>
+        <div className="ghost__eyes">
+          <div className="ghost__eyes_eye"></div>
+          <div className="ghost__eyes_eye"></div>
+        </div>
+        <div className="ghost__mouth"></div>
+      </div>
+    </div>
+  );
+}
