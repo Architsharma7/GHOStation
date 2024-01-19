d229d027f8d6098e21bf0dca6809691f2fc28bf4
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2024-01-19 12:53:14 +0530

    feat: OZ remmappings added

--- contracts/src/ERC4626Vault.sol
+++ contracts/src/ERC4626Vault.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
+import "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-contract ERC4626Vault {
+contract ERC4626Vault is IERC4626 {
     constructor() {}
 }
