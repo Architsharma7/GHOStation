bd6a742988c0b740f4e9833f71a409e2853e814f
Architsharma7 <architsharma7911@gmail.com>
2024-01-21 15:58:35 +0530

    feat: ipfsstorage functions added

---
+++ app/utils/ipfsstorage.ts
@@ -0,0 +1,16 @@
+import lighthouse from "@lighthouse-web3/sdk";
+
+export const uploadProposal = async (obj: Object) => {
+  const blob = new Blob([JSON.stringify(obj)], { type: "application/json" });
+  const file = [new File([blob], "proposal.json")];
+  const output = await lighthouse.upload(file, "685a8211.cacf55559b244372862ac7d68c56cba6", true, undefined);
+  console.log(
+    "Visit at https://gateway.lighthouse.storage/ipfs/" + output.data[0].Hash
+  );
+  console.log(output);
+  return output.data[0].Hash;
+};
+
+export const getFile = async (hash: string) => {
+    const fileInfo = await lighthouse.getFileInfo(hash);
+}
